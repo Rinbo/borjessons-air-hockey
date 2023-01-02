4b535345f0f4c4ed171ac91a7601dd7102292cc4
Robin Börjesson <robin.b@outlook.com>
2023-01-02 21:16:54 +0100

    Move stuff around, create structure, add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import Start from "./views/start/start";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./css/index.css";
+import ErrorPage from "./views/error/error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/views/start/start.tsx
@@ -0,0 +1,7 @@
+export default function Start() {
+  return (
+    <div className="w-full">
+      <div className="text-3xl font-bold underline text-center">Hello</div>
+    </div>
+  );
+}
