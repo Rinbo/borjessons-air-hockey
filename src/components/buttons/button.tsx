886e462174c4509d9d0b4e9dd5bf4c38b6e14b41
Robin Börjesson <robin.b@outlook.com>
2023-01-04 23:14:21 +0100

    Add ugly css puck animation

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -2,7 +2,7 @@ type Props = { text: string; children: React.ReactNode };
 
 export default function Button({ text, children }: Props) {
   return (
-    <button className="bg-bg hover:bg-teal-200 text-primary font-bold font-mono py-2 px-4 w-56 rounded inline-flex justify-center">
+    <button className="hover:bg-teal-200 z-10 text-primary font-bold py-2 px-4 w-56 rounded inline-flex justify-center">
       {children}
       <span>{text}</span>
     </button>
--- src/routes/landing/landing-page.tsx
+++ src/routes/landing/landing-page.tsx
@@ -1,18 +1,23 @@
-import Button from "../../components/buttons/button";
 import JoinButton from "../../components/buttons/join-button";
 import OnlineButton from "../../components/buttons/online-button";
 import PlayButton from "../../components/buttons/play-button";
+import "../../css/landing-bg.css";
 
 export default function LandingPage() {
   return (
-    <div className="h-screen flex flex-col bg-bg text-primary">
-      <div className="text-3xl text-center font-mono  pt-20">borjessons</div>
-      <div className="text-6xl text-center font-bold">Air Hockey</div>
+    <div className="bg-bg">
+      <div className="w-screen h-screen flex flex-col text-primary">
+        <div className="text-3xl text-center pt-20">borjessons</div>
+        <div className="text-6xl text-center font-bold">Air Hockey</div>
 
-      <div className="flex flex-col grow items-center justify-center gap-2 ">
-        <PlayButton />
-        <JoinButton />
-        <OnlineButton />
+        <div className="flex flex-col grow items-center justify-center gap-2 ">
+          <PlayButton />
+          <JoinButton />
+          <OnlineButton />
+        </div>
+        <ul className="circles">
+          <li></li>
+        </ul>
       </div>
     </div>
   );
