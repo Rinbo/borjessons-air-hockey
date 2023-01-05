861763d751045a3631a1da79f09b5162565fc32c
Robin Börjesson <robin.b@outlook.com>
2023-01-05 22:39:49 +0100

    Add ugly skewing effect on title when enter

--- src/routes/landing/landing-page.tsx
+++ src/routes/landing/landing-page.tsx
@@ -8,11 +8,11 @@ export default function LandingPage() {
     <div className="bg-bg">
       <div className="w-screen h-screen flex flex-col text-primary">
         <div className="text-3xl text-center pt-20">borjessons</div>
-        <div className="text-6xl text-center font-bold">Air Hockey</div>
+        <div className="text-6xl text-center font-bold ripple">Air Hockey</div>
 
         <div className="flex flex-col grow items-center justify-center gap-2 ">
-          <PlayButton />
           <JoinButton />
+          <PlayButton />
           <OnlineButton />
         </div>
         <ul className="puck">
