c89b066fbcce457a6b18320302bf1f1e74a1751e
Robin Börjesson <robin.b@outlook.com>
2023-01-12 23:25:00 +0100

    Finally fix working chat in lobby using stomp and sockJs

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,44 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import GamesLayout from "./routes/games/layout";
-import ErrorPage from "./routes/error/error-page";
-import Lobby from "./routes/games/lobby";
-import LandingPage from "./routes/landing/landing-page";
-import ChooseAName from "./routes/choose-a-name/choose-a-name";
-import GenerateRoom from "./routes/games/generate-room";
-import "./css/index.css";
-import AvailableGames from "./routes/games/available-games";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import GamesLayout from './routes/games/layout';
+import ErrorPage from './routes/error/error-page';
+import Lobby from './routes/games/lobby';
+import LandingPage from './routes/landing/landing-page';
+import ChooseAName from './routes/choose-a-name/choose-a-name';
+import GenerateRoom from './routes/games/generate-room';
+import './css/index.css';
+import AvailableGames from './routes/games/available-games';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <LandingPage />,
-    errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />
   },
   {
-    path: "/online",
-    element: <h1 className="text-3xl text-center pt-8">Online Users</h1>,
+    path: '/online',
+    element: <h1 className="text-3xl text-center pt-8">Online Users</h1>
   },
   {
-    path: "/choose-a-name",
-    element: <ChooseAName />,
+    path: '/choose-a-name',
+    element: <ChooseAName />
   },
   {
-    path: "/games",
+    path: '/games',
     element: <GamesLayout />,
     children: [
       { index: true, element: <AvailableGames /> },
-      { path: "new", element: <GenerateRoom /> },
-      { path: ":id", element: <Lobby /> },
-    ],
-  },
+      { path: 'new', element: <GenerateRoom /> },
+      { path: ':id', element: <Lobby /> }
+    ]
+  }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <div className="text-primary bg-bg h-min w-screen">
-      <RouterProvider router={router} />
-    </div>
-  </React.StrictMode>
+document.cookie = `SameSite=None;Secure`;
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <div className="text-primary bg-bg h-min w-screen">
+    <RouterProvider router={router} />
+  </div>
 );
--- src/routes/games/lobby.tsx
+++ src/routes/games/lobby.tsx
@@ -1,37 +1,41 @@
-import React from "react";
-import { useOutletContext, useParams } from "react-router-dom";
-import IconButton from "../../components/buttons/icon-button";
-import shareIcon from "../../assets/svg/share.svg";
-import sendIcon from "../../assets/svg/send.svg";
+import React from 'react';
+import { useOutletContext, useParams } from 'react-router-dom';
+import IconButton from '../../components/buttons/icon-button';
+import shareIcon from '../../assets/svg/share.svg';
+import sendIcon from '../../assets/svg/send.svg';
+import SockJS from 'sockjs-client/dist/sockjs';
+import Stomp from 'stompjs';
 
 type Message = { username: string; message: string; datetime: string };
 type OutletContext = { name: string };
-const CHAT = ["Waiting for opponent to join...", "Opponent joined..."];
-
-let stompClient: any;
+const CHAT = ['Waiting for opponent to join...', 'Opponent joined...'];
 
 export default function Lobby() {
   const { id } = useParams<string>();
   const { name } = useOutletContext<OutletContext>();
+  const [stompClient, setStompClient] = React.useState<Stomp.Client | null>();
   const [messages, setMessages] = React.useState<Array<Message>>([]);
+  const [message, setMessage] = React.useState<string>('');
 
   React.useEffect(() => {
-    if (!stompClient) connect();
-  }, []);
+    const socket = new SockJS('http://localhost:8080/ws');
+    const stomp = Stomp.over(socket);
 
-  function connect() {
-    const Stomp = require("stompjs");
-    let SockJS = require("sockjs-client");
-
-    const socket = new SockJS("http://localhost:8080/air-hockey-server");
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame: string) {
-      console.log("Connected: " + frame);
-      stompClient.subscribe("/topic/public", function (message: Message) {
-        setMessages((prevState) => [...prevState, message]);
+    stomp.connect({}, frame => {
+      console.log('Connecting', frame);
+      setStompClient(stomp);
+      stomp.subscribe('/topic/public', (message: Stomp.Message) => {
+        setMessages(prev => [...prev, JSON.parse(message.body) as Message]);
       });
     });
-  }
+
+    return () => stomp.disconnect(() => console.log('disconnecting...'));
+  }, []);
+
+  const sendMessage = () => {
+    stompClient && stompClient.send('/app/send-message', {}, JSON.stringify({ username: name, message: message, datetime: new Date().toDateString() }));
+    setMessage('');
+  };
 
   return (
     <div className="sm:container sm:max-w-3xl h-screen sm:mx-auto px-2 py-4">
@@ -41,40 +45,30 @@ export default function Lobby() {
           <div className="flex items-center gap-2">
             <button className="btn btn-primary-outlined">Ready</button>
             <div className="inline-flex flex-col gap-2">
-              <span className="text-xs whitespace-nowrap overflow-hidden">
-                P1: {name}
-              </span>
-              <span className="text-xs whitespace-nowrap overflow-hidden">
-                P2:
-              </span>
+              <span className="text-xs whitespace-nowrap overflow-hidden">P1: {name}</span>
+              <span className="text-xs whitespace-nowrap overflow-hidden">P2:</span>
             </div>
           </div>
           <div className="flex gap-2">
-            <IconButton
-              text=""
-              onClick={() => console.log("share")}
-              className="border border-primary"
-              svgIcon={shareIcon}
-            />
+            <IconButton text="" onClick={() => console.log('share')} className="border border-primary" svgIcon={shareIcon} />
             <button className="btn btn-orange-outlined w-20">Exit</button>
           </div>
         </div>
         <div className="flex-1 flex flex-col justify-end bg-slate-100 p-2 border-2 border-primary border-opacity-50 rounded-md ">
-          {CHAT.map((line) => (
-            <span>{line}</span>
+          {messages.map((line, index) => (
+            <div className="flex gap-2" key={index}>
+              <span>{line.username}:</span>
+              <span className="ml-2">{line.message}</span>
+            </div>
           ))}
         </div>
         <input
           placeholder="Write something..."
           className="p-2 bg-slate-100 border-2 border-primary border-opacity-40 rounded"
+          onChange={e => setMessage(e.target.value)}
         />
         <div className="flex justify-end">
-          <IconButton
-            text=""
-            onClick={() => console.log("Send")}
-            className="border border-primary"
-            svgIcon={sendIcon}
-          />
+          <IconButton text="" onClick={sendMessage} className="border border-primary" svgIcon={sendIcon} />
         </div>
       </div>
     </div>
