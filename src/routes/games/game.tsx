f27888b75016d4d2808b606f70fe45fdcb24d16a
Robin Börjesson <robin.b@outlook.com>
2023-02-04 11:20:47 +0100

    Add comment plan

--- src/routes/games/game.tsx
+++ src/routes/games/game.tsx
@@ -19,8 +19,6 @@ export default function Game() {
   const board = React.useRef<Board>();
   const lastFrame = React.useRef<number>(performance.now());
 
-  //useInterval(gameLoop, 1000 / FPS);
-
   React.useEffect(() => {
     const canvas = document.getElementById('game-board') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -50,39 +48,3 @@ export default function Game() {
     </div>
   );
 }
-
-/**
-  const [context, setContext] = React.useState<CanvasRenderingContext2D>();
-  const [cooldown, setCooldown] = React.useState<number>(0);
-  const [position, setPosition] = React.useState<Position>({ x: 30, y: 30 });
-  const [speed, setSpeed] = React.useState<Speed>({ x: 5, y: 5 });
-
-  function gameLoop() {
-    context?.clearRect(0, 0, width, height);
-    context?.fillRect(position.x, position.y, 30, 30);
-    checkCollision();
-    setPosition(updatePos);
-    setCooldown(prev => Math.max(0, prev - 1));
-  }
-
-  function checkCollision(): void {
-    const { x: xPos, y: yPos } = position;
-
-    if ((xPos + S_LEN >= width || xPos <= 0) && cooldown === 0) setSpeed(reverseXSpeed);
-    if ((yPos + S_LEN >= height || yPos <= 0) && cooldown === 0) setSpeed(reverseYSpeed);
-  }
-
-  function reverseXSpeed({ x, y }: Speed): Speed {
-    setCooldown(10);
-    return { x: x * -1, y };
-  }
-
-  function reverseYSpeed({ x, y }: Speed): Speed {
-    setCooldown(10);
-    return { x, y: y * -1 };
-  }
-
-  function updatePos({ x, y }: Position): Position {
-    return { x: x + speed.x, y: y + speed.y };
-  }
- */
