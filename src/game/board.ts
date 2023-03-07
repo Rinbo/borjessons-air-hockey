0fd96851b0bbe191200013c67d9a577f0fdce9f4
Robin Börjesson <robinborjesson@macbook-pro.local>
2023-03-07 21:52:05 +0100

    Add canvas goal indicators

--- src/game/board.ts
+++ src/game/board.ts
@@ -1,4 +1,4 @@
-import { OPPONENT_HANDLE_START_POS, PLAYER_HANDLE_START_POS } from './constants';
+import { GOAL_ANGLE, GOAL_HEIGHT, GOAL_WIDTH, OPPONENT_HANDLE_START_POS, PLAYER_HANDLE_START_POS } from './constants';
 import OpponentHandle from './opponent-handle';
 import PlayerHandle from './player-handle';
 import Puck from './puck';
@@ -36,6 +36,8 @@ export default class Board {
   public draw(): void {
     this.ctx.clearRect(0, 0, this.size.width, this.size.height);
     this.centerLine();
+    this.goal(this.bottomGoal);
+    this.goal(this.topGoal);
     [this.playerHandle, this.opponentHandle, this.puck].map(e => e.draw());
   }
 
@@ -72,5 +74,29 @@ export default class Board {
     this.ctx.moveTo(0, this.canvas.height / 2);
     this.ctx.lineTo(this.canvas.width, this.canvas.height / 2);
     this.ctx.stroke();
+  }
+
+  private goal(callback: (width: number, height: number, ctx: CanvasRenderingContext2D) => void): void {
+    const { width, height } = this.size;
+    this.ctx.lineWidth = 1;
+    this.ctx.fillStyle = 'rgb(0,0,0,0.2)';
+    this.ctx.beginPath();
+    callback(width, height, this.ctx);
+    this.ctx.stroke();
+    this.ctx.fill();
+    this.ctx.fillStyle = 'rgb(0,0,0,1)';
+  }
+
+  private topGoal(width: number, height: number, ctx: CanvasRenderingContext2D): void {
+    ctx.moveTo(width / 2 - GOAL_WIDTH * width, 0);
+    ctx.lineTo(width / 2 - GOAL_WIDTH * width + GOAL_ANGLE * width, GOAL_HEIGHT * height);
+    ctx.lineTo(width / 2 + GOAL_WIDTH * width - GOAL_ANGLE * width, GOAL_HEIGHT * height);
+    ctx.lineTo(width / 2 + GOAL_WIDTH * width, 0);
+  }
+  private bottomGoal(width: number, height: number, ctx: CanvasRenderingContext2D): void {
+    ctx.moveTo(width / 2 - GOAL_WIDTH * width, height);
+    ctx.lineTo(width / 2 - GOAL_WIDTH * width + GOAL_ANGLE * width, height - GOAL_HEIGHT * height);
+    ctx.lineTo(width / 2 + GOAL_WIDTH * width - GOAL_ANGLE * width, height - GOAL_HEIGHT * height);
+    ctx.lineTo(width / 2 + GOAL_WIDTH * width, height);
   }
 }
