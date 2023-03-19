e89e75b0b096fdf43a8dbf290f640f21b4dfbf4e
Robin Börjesson <robinb@outlook.com>
2023-03-19 17:02:49 +0100

    No games gives option to create one

--- src/routes/games/available-games.tsx
+++ src/routes/games/available-games.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 type Row = { id: string; username: string; joinable: boolean };
@@ -12,35 +13,50 @@ const DUMMY_DATA = [
 
 export default function AvailableGames() {
   const navigate = useNavigate();
+  const [games, setGames] = React.useState<Array<Row>>(DUMMY_DATA);
 
   function renderAvailableGames() {
-    return DUMMY_DATA.filter(game => game.joinable).map(row => {
-      return (
-        <div
-          key={row.id}
-          className="flex items-center justify-between rounded-md border-black bg-slate-300 px-2 py-2 duration-300 hover:scale-105 hover:transform hover:border hover:bg-teal-100"
-        >
-          <span className="font-bold">{`${row.username}'s game`}</span>
-          <button
-            className="btn border border-primary bg-teal-200 text-primary hover:bg-teal-300 hover:text-white"
-            onClick={() => navigate('/games/' + row.id)}
+    return games
+      .filter(game => game.joinable)
+      .map(row => {
+        return (
+          <div
+            key={row.id}
+            className="flex items-center justify-between rounded-md border-black bg-slate-300 px-3 py-2 duration-300 hover:scale-105 hover:transform hover:border hover:bg-teal-100"
           >
-            Join
-          </button>
-        </div>
-      );
-    });
+            <span className="font-bold">{`${row.username}'s game`}</span>
+            <button
+              className="btn border border-primary bg-teal-200 text-primary hover:bg-teal-300 hover:text-white"
+              onClick={() => navigate('/games/' + row.id)}
+            >
+              Join
+            </button>
+          </div>
+        );
+      });
   }
 
+  const renderNoGamesAvailable = () => {
+    if (games.length !== 0) return null;
+    return (
+      <div className="flex flex-col justify-center gap-2">
+        <div className="text-center">No games available</div>
+        <button className="btn btn-primary-outlined">Create One</button>
+      </div>
+    );
+  };
+
   function renderFullGames() {
-    return DUMMY_DATA.filter(game => !game.joinable).map(row => {
-      return (
-        <div key={row.id} className="flex items-center justify-between rounded-md border-black bg-slate-300 px-2 py-2">
-          <span className="font-bold">{`${row.username}'s game`}</span>
-          <button className="btn cursor-not-allowed border border-primary bg-red-300">Game in progress</button>
-        </div>
-      );
-    });
+    return games
+      .filter(game => !game.joinable)
+      .map(row => {
+        return (
+          <div key={row.id} className="flex items-center justify-between rounded-md border-black bg-slate-300 px-3 py-2">
+            <span className="font-bold">{`${row.username}'s game`}</span>
+            <button className="btn cursor-not-allowed border border-primary bg-red-300">In progress</button>
+          </div>
+        );
+      });
   }
 
   return (
@@ -48,6 +64,7 @@ export default function AvailableGames() {
       <div className="flex h-full flex-col gap-1">
         <div className="pb-4 pt-12 text-center text-3xl">Available Games</div>
         <div className="flex flex-col gap-2 rounded-lg border border-primary p-3">
+          {renderNoGamesAvailable()}
           {renderAvailableGames()}
           {renderFullGames()}
         </div>
