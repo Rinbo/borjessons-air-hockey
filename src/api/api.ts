7268de6d86064f3601ce593b4c757f3a9f5ac445
Robin Börjesson <robin.b@outlook.com>
2023-03-25 14:44:25 +0100

    Add remeining api methods, fix layout to validate name

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,19 +8,51 @@ interface RequestOptions {
   body?: string;
 }
 
-export async function post<T>(path: string, data: T): Promise<T> {
+const statusError = (status: number) => new Error(`HTTP error! status: ${status}`);
+
+export async function get<T>(path: string): Promise<T> {
   const { restBaseUrl } = properties();
-  const requestOptions: RequestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  };
+  const requestOptions: RequestOptions = getRequestOptions('GET');
 
   const response = await fetch(restBaseUrl + path, requestOptions);
 
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    throw statusError(response.status);
   }
 
   return await response.json();
+}
+
+export async function post<T>(path: string, data: T): Promise<T> {
+  const { restBaseUrl } = properties();
+  const requestOptions: RequestOptions = getRequestOptions('POST', data);
+
+  const response = await fetch(restBaseUrl + path, requestOptions);
+
+  if (!response.ok) {
+    throw statusError(response.status);
+  }
+
+  return await response.json();
+}
+
+export async function put<T>(path: string, data: T): Promise<T> {
+  const { restBaseUrl } = properties();
+  const requestOptions: RequestOptions = getRequestOptions('PUT', data);
+
+  const response = await fetch(restBaseUrl + path, requestOptions);
+
+  if (!response.ok) {
+    throw statusError(response.status);
+  }
+
+  return await response.json();
+}
+
+function getRequestOptions<T>(httpMethod: HttpMethod, data?: T): RequestOptions {
+  return {
+    method: httpMethod,
+    headers: { 'Content-Type': 'application/json' },
+    body: data ? JSON.stringify(data) : undefined
+  };
 }
--- src/routes/games/layout.tsx
+++ src/routes/games/layout.tsx
@@ -4,12 +4,13 @@ import Banner from '../../components/misc/banner';
 import SockJS from 'sockjs-client/dist/sockjs';
 import Stomp from 'stompjs';
 import properties from '../../config/properties';
+import { get } from '../../api/api';
 
 export default function GamesLayout() {
   const { wsBaseUrl } = properties();
   const savedUsername = localStorage.getItem('username');
   const [stompClient, setStompClient] = React.useState<Stomp.Client | null>();
-  const [username, setUsername] = React.useState();
+  const [username, setUsername] = React.useState<string>();
 
   React.useEffect(() => {
     const socket = new SockJS(wsBaseUrl);
@@ -17,12 +18,16 @@ export default function GamesLayout() {
     client.connect({}, _frame => setStompClient(client));
   }, []);
 
-  // TODO rename username from localStorage to savedUsername. If it is not set then forward to set-name component
-  // If it is set (meaning we were redirected back here) then first validate, enter via websocket and then set state username which also cannot be null
   React.useEffect(() => {
-    stompClient && savedUsername && stompClient.send('/app/enter', {}, savedUsername);
-    return () => stompClient && savedUsername && stompClient.send('/app/exit', {}, savedUsername);
-  }, [savedUsername]);
+    savedUsername &&
+      get<string>(`/users/${savedUsername}/validate`).then(data => {
+        console.log(data, 'Received validated username');
+        setUsername(data);
+        stompClient && stompClient.send('/app/enter', {}, data);
+      });
+
+    return () => stompClient && username && stompClient.send('/app/exit', {}, username);
+  }, [stompClient]);
 
   const renderConnecting = (
     <div className="flex h-screen items-center justify-center">
