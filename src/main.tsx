310b2173324d145d4449b8f4fa9813dffd865e96
Robin Börjesson <robin.b@outlook.com>
2023-04-02 19:40:59 +0200

    Switch to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import GamesLayout from './routes/games/layout';
 import ErrorPage from './routes/error/error-page';
@@ -12,37 +12,32 @@ import GameContainer from './routes/games/game-container';
 import TestGame from './routes/games/test-game';
 import { OnlineUsers } from './routes/games/online-users';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <LandingPage />,
-      errorElement: <ErrorPage />
-    },
-    {
-      path: '/choose-a-name',
-      element: <ChooseAName />
-    },
-    {
-      path: '/test',
-      element: <TestGame />
-    },
-    {
-      path: '/games',
-      element: <GamesLayout />,
-      errorElement: <ErrorPage />,
-      children: [
-        { index: true, element: <AvailableGames /> },
-        { path: 'new', element: <GenerateRoom /> },
-        { path: ':id', element: <GameContainer /> },
-        { path: 'online', element: <OnlineUsers /> }
-      ]
-    }
-  ],
+const router = createHashRouter([
   {
-    basename: '/borjessons-air-hockey'
+    path: '/',
+    element: <LandingPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/choose-a-name',
+    element: <ChooseAName />
+  },
+  {
+    path: '/test',
+    element: <TestGame />
+  },
+  {
+    path: '/games',
+    element: <GamesLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <AvailableGames /> },
+      { path: 'new', element: <GenerateRoom /> },
+      { path: ':id', element: <GameContainer /> },
+      { path: 'online', element: <OnlineUsers /> }
+    ]
   }
-);
+]);
 
 document.cookie = `SameSite=None;Secure`;
 
